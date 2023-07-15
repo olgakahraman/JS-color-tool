ea8ad622fa3ddfc7b5e2adf40d39f731189a4cdd
olgakahraman <ol.khrmn@gmail.com>
2023-07-14 22:11:57 -0400

    display color from user hex input

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+const hexInput = document.querySelector("#hexInput");
+const inputColor = document.querySelector("#inputColor");
+
+hexInput.addEventListener("keyup", () => {
+  const hex = hexInput.value;
+  if(!isValidHex(hex)) return;
+
+  const strippedHex = hex.replace("#", "");
+
+
+  inputColor.style.backgroundColor = "#" + strippedHex;
+});
+
 const isValidHex = (hex) => {
   if (!hex) return false;
 
   const srtippedHex = hex.replace("#", "");
   return srtippedHex.length === 3 || srtippedHex.length === 6;
 };
-
-
