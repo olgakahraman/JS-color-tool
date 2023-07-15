3520da877ec702d17b5b56c5d8f7dca58892afa0
olgakahraman <ol.khrmn@gmail.com>
2023-07-14 23:02:49 -0400

    display % from slider

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const hexInput = document.querySelector("#hexInput");
 const inputColor = document.querySelector("#inputColor");
+const slider = document.querySelector("#slider");
+const sliderText = document.querySelector("#sliderText");
+
 
 hexInput.addEventListener("keyup", () => {
   const hex = hexInput.value;
@@ -45,4 +48,9 @@ const convertRGBToHex = (r,g,b) => {
     const hex = "#" + firstPair + secondPair + thirdPair;
     return hex;
 }
-console.log(convertRGBToHex(255, 255, 255));
+
+
+
+slider.addEventListener("input", () => {
+    sliderText.textContent = `${slider.value}%`;
+})
